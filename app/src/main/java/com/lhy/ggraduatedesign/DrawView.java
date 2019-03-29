19d7221e762dc02595397a05218428eb994268b0
ylzz1997 <ylzz1997@qq.com>
2019-03-29 21:44:04 +0800

    没啥可说的，更新

---
+++ app/src/main/java/com/lhy/ggraduatedesign/DrawView.java
@@ -0,0 +1,132 @@
+package com.lhy.ggraduatedesign;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+
+public class DrawView extends View implements View.OnTouchListener {
+    float PI=(float)Math.PI;//PI=3.1415那个什么的每次调用(float)Math.PI太麻烦，自定义一个。
+    float canvasWidth,canvasHeight;//画布宽、高
+    float width,height;//自定义长宽
+    float left,up;//自定义左上角位置
+    public DrawView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas){
+        super.onDraw(canvas);
+        //开始一定要先调用Init()初始化参数。我自定义的参数没有默认值。
+        Init(canvas);
+        Render(canvas);
+    }
+    @Override
+    public boolean onTouch(View view, MotionEvent event) {
+        return false;
+    }
+    //初始化全局参数。
+    //建议不要在其它函数里更改在这里初始化的参数，以免得不出正确结果
+    void Init(Canvas canvas){
+        //获取画布宽、高
+        canvasWidth=(float)canvas.getWidth();
+        canvasHeight=(float)canvas.getHeight();
+        //自定义长宽
+        width=2*PI;height=2*PI*canvasHeight/canvasWidth;
+        // 自定义左上角位置
+        left=-width/2;up=height/2;
+    }
+    //渲染画面。
+    //画图步骤在这里。
+    void Render(Canvas canvas){
+        //新建画笔
+        Paint paint=new Paint();
+        //画网格线。
+        paint.setARGB(255, 255, 0, 0);
+        DrawGrid(canvas,0.3f,0.3f,paint);
+        //画坐标线。
+        paint.setARGB(255,0,0,0);
+        DrawCoord(canvas,paint);
+        //显示横、纵坐标轴名称和原点名称。(重要提示:这里就是你想要的新东西)
+        paint.setARGB(255,100,100,100);
+        DrawCoordName(canvas,paint,"t","f(t)","O");
+        //画曲线
+        DrawCurve(canvas, paint);
+    }
+    //画网格线。
+    //要先画网格再画坐标，不然网格线会把坐标线覆盖掉从而看不到坐标线
+    //dx,dy:
+    void DrawGrid(Canvas canvas,float dx,float dy,Paint paint){
+        //画纵向网格线
+        //左侧
+        float x=0;
+        while (x>left){
+            canvas.drawLine(PX(x),PY(up),PX(x),PY(up-height),paint);
+            x-=dx;
+        }
+        //右侧
+        x=0;
+        while (x<width+left){
+            canvas.drawLine(PX(x),PY(up),PX(x),PY(up-height),paint);
+            x+=dx;
+        }
+        //画横向网格线
+        //上侧
+        float y=0;
+        while (y<up){
+            canvas.drawLine(PX(left),PY(y),PX(left+width),PY(y),paint);
+            y+=dy;
+        }
+        //下侧
+        y=0;
+        while (y>up-height){
+            canvas.drawLine(PX(left),PY(y),PX(left+width),PY(y),paint);
+            y-=dy;
+        }
+    }
+    //画坐标线。
+    void DrawCoord(Canvas canvas,Paint paint){
+        //画x,y坐标
+        canvas.drawLine(PX(left),PY(0f),PX(left+width),PY(0f),paint);
+        canvas.drawLine(PX(0f),PY(up),PX(0f),PY(up-height),paint);
+    }
+    //(重要提示:这里就是你想要的新东西)
+    //显示横、纵坐标轴名称和原点名称。
+    void DrawCoordName(Canvas canvas,Paint paint,String xAxisName,String yAxisName,String originName){
+        //设置文字大小
+        paint.setTextSize(40f);
+        //在适当位置显示x,y,O名称
+        canvas.drawText(xAxisName,PX(left+width)-30f,PY(0f)+30f,paint);
+        canvas.drawText(yAxisName,PX(0f),PY(up)+30f,paint);
+        canvas.drawText(originName,PX(0f),PY(0f)+30f,paint);
+
+    }
+    //画点函数。使用自定义坐标。
+    void DrawPoint(Canvas canvas,float x,float y,Paint paint){
+        canvas.drawPoint(PX(x),PY(y),paint);
+    }
+    //(重要提示:所有你想画的曲线都在这里进行。)
+    //画曲线函数。
+    //使用画布坐标。所以要调用PX(x),PY(y)把自定义坐标里的量(如x,y)转换成画布坐标。
+    void DrawCurve(Canvas canvas,Paint paint){//绘制曲线
+
+        paint.setARGB(255,0,0,255);
+            for(float x=left;x<left+width;x+=0.001f){
+                float y=(float)Math.sin(x);
+            DrawPoint(canvas,x,y,paint);
+        }
+        //因为x,y是自定义坐标，canvas.drawText()是系统提供的函数，
+        // 所以要调用PX(),PY()将(PI/2,sin(PI/2))转换成画布坐标；
+        canvas.drawText("f(t)=sin(t)",PX(PI/2),PY((float)Math.sin(PI/2)),paint);
+    }
+    //将自定义坐标转换成画布坐标的函数，
+    //当要用自定义坐标在系统提供的函数上绘图时，要调用这两个函数把自定义坐标转换成画布坐标
+    float PX(float x){
+        return (x-left)*canvasWidth/width;
+    }
+    float PY(float y){
+        return (up-y)*canvasHeight/height;
+    }
+}
--- app/src/main/java/com/lhy/ggraduatedesign/MainActivity.java
+++ app/src/main/java/com/lhy/ggraduatedesign/MainActivity.java
@@ -1,28 +1,50 @@
 package com.lhy.ggraduatedesign;
 
+        import android.content.SharedPreferences;
+        import android.support.v7.app.AlertDialog;
         import android.support.v7.app.AppCompatActivity;
         import android.os.Bundle;
+        import android.view.LayoutInflater;
         import android.view.MotionEvent;
         import android.view.View;
         import android.widget.Button;
+        import android.widget.EditText;
+        import android.widget.LinearLayout;
         import android.widget.TextView;
         import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    SharedPreferences settings;
     TextView tv;
     DateProcessor dp;
     SensorUtil su;
     Button bt;
+    Button bto;
+    Button setb;
+    float body_height = 1.5f;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        settings = getSharedPreferences("setting", 0);
+        body_height = Float.parseFloat(settings.getString("bh","1.5"));
+        bto = findViewById(R.id.button_one);
         bt = findViewById(R.id.main_b1);
+        setb = findViewById(R.id.setting);
         bt.setOnTouchListener((v,e) -> {
             onButtonClick(v,e);
             return false;
         });
+        bto.setOnTouchListener((v,e) -> {
+            onButtonClick(v,e);
+            return false;
+        });
+        setb.setOnClickListener((v -> {
+            showDialog();
+        }));
+
         su = new SensorUtil(this);
     }
 
@@ -57,4 +79,39 @@ public class MainActivity extends AppCompatActivity {
     public void setRequestedOrientation(int requestedOrientation){
         return;
     }
+
+
+    private void showDialog(){
+        View view = LayoutInflater.from(this).inflate(R.layout.body_height,null,false);
+        final AlertDialog dialog = new AlertDialog.Builder(this).setView(view).create();
+
+        Button btn_cancel_high_opion = view.findViewById(R.id.bh_ok);
+        Button btn_agree_high_opion = view.findViewById(R.id.bh_cancel);
+        EditText bh = view.findViewById(R.id.editText);
+        bh.setText(String.valueOf(body_height));
+        btn_cancel_high_opion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                body_height = Float.parseFloat(bh.getText().toString());
+                settings.edit().putString("bh",bh.getText().toString()).commit();
+                //... To-do
+                dialog.dismiss();
+            }
+        });
+
+        btn_agree_high_opion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //... To-do
+                dialog.dismiss();
+            }
+        });
+
+        dialog.show();
+        //此处设置位置窗体大小，我这里设置为了手机屏幕宽度的3/4
+        dialog.getWindow().setLayout((this.getResources().getDisplayMetrics().widthPixels/4*3), LinearLayout.LayoutParams.WRAP_CONTENT);
+    }
+
 }
+
+
